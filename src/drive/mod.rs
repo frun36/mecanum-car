033e874dc198abf20cf1b3a508d1b5dc9a8026b5
frun36 <franek.urbanski@gmail.com>
2023-06-15 10:55:28 +0200

    Configured cross-compilation. Separated code into modules. Added `drive` module for easier control

---
+++ src/drive/mod.rs
@@ -0,0 +1,55 @@
+use rppal::gpio::Gpio;
+
+mod motor;
+
+pub struct Drive {
+    motors: [motor::Motor; 4],
+    pwm_frequency: f64,
+}
+
+impl Drive {
+    pub fn new(gpio: &Gpio, motor_pins: [(u8, u8, u8); 4], pwm_frequency: f64) -> Self {
+        Self {
+            motors: [
+                motor::Motor::new(&gpio, motor_pins[0].0, motor_pins[0].1, motor_pins[0].2),
+                motor::Motor::new(&gpio, motor_pins[1].0, motor_pins[1].1, motor_pins[1].2),
+                motor::Motor::new(&gpio, motor_pins[2].0, motor_pins[2].1, motor_pins[2].2),
+                motor::Motor::new(&gpio, motor_pins[3].0, motor_pins[3].1, motor_pins[3].2),
+            ],
+            pwm_frequency,
+        }
+    }
+
+    pub fn fwd(&mut self, speed: f64) {
+        for m in self.motors.iter_mut() {
+            m.enable_fwd(self.pwm_frequency, speed);
+        }
+    }
+
+    pub fn bwd(&mut self, speed: f64) {
+        for m in self.motors.iter_mut() {
+            m.enable_bwd(self.pwm_frequency, speed);
+        }
+    }
+
+    pub fn rwd(&mut self, speed: f64) {
+        self.motors[0].enable_bwd(self.pwm_frequency, speed);
+        self.motors[1].enable_fwd(self.pwm_frequency, speed);
+        self.motors[2].enable_bwd(self.pwm_frequency, speed);
+        self.motors[3].enable_fwd(self.pwm_frequency, speed);
+    }
+
+    pub fn lwd(&mut self, speed: f64) {
+        self.motors[0].enable_fwd(self.pwm_frequency, speed);
+        self.motors[1].enable_bwd(self.pwm_frequency, speed);
+        self.motors[2].enable_fwd(self.pwm_frequency, speed);
+        self.motors[3].enable_bwd(self.pwm_frequency, speed);
+    }
+
+    pub fn stop(&mut self) {
+        for m in self.motors.iter_mut() {
+            m.stop();
+        }
+    }
+}
+
---
+++ src/drive/motor.rs
@@ -0,0 +1,51 @@
+use rppal::gpio::Gpio;
+use rppal::gpio::OutputPin;
+
+pub struct Motor {
+    fwd_pin: OutputPin,
+    bwd_pin: OutputPin,
+    pwm_pin: OutputPin,
+}
+
+impl Motor {
+    pub fn new(gpio: &Gpio, fwd_pin_number: u8, bwd_pin_number: u8, pwm_pin_number: u8) -> Self {
+        Self {
+            fwd_pin: gpio
+                .get(fwd_pin_number)
+                .expect("Error: Couldn't get gpio")
+                .into_output(),
+            bwd_pin: gpio
+                .get(bwd_pin_number)
+                .expect("Error: Couldn't get gpio")
+                .into_output(),
+            pwm_pin: gpio
+                .get(pwm_pin_number)
+                .expect("Error: Couldn't get gpio")
+                .into_output(),
+        }
+    }
+
+    pub fn enable_fwd(&mut self, pwm_frequency: f64, duty_cycle: f64) {
+        self.bwd_pin.set_low();
+        self.pwm_pin.set_high();
+        self.fwd_pin.set_high();
+        self.pwm_pin
+            .set_pwm_frequency(pwm_frequency, duty_cycle)
+            .expect("Counldn't set PWM frequency");
+    }
+
+    pub fn enable_bwd(&mut self, pwm_frequency: f64, duty_cycle: f64) {
+        self.fwd_pin.set_low();
+        self.pwm_pin.set_high();
+        self.bwd_pin.set_high();
+        self.pwm_pin
+            .set_pwm_frequency(pwm_frequency, duty_cycle)
+            .expect("Counldn't set PWM frequency");
+    }
+
+    pub fn stop(&mut self) {
+        self.pwm_pin.set_low();
+        self.fwd_pin.set_low();
+        self.bwd_pin.set_low();
+    }
+}
